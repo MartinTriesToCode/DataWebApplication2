9907035f2fb402a2c55790cfc028d173ddf6ed7e
Martin Lundgren <93119436+MartinTriesToCode@users.noreply.github.com>
2022-04-17 02:31:33 +0200

    Add files via upload

---
+++ DataWebApplication/Models/Entities/ReactModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataWebApplication.Models.Entities
+{
+    public class ReactModel
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<string> Cities { get; set; }
+        public List<string> Countries { get; set; }
+        public List<string> Languages { get; set; }
+        public List<string> LanguageList { get; set; }
+    }
+}
